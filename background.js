b6ae66ffe0a0fa31616949d3f438ed559fc54b07
FoxickL <FoxickL@users.noreply.github.com>
2026-04-11 10:27:08 +0000

    deploy: 4f64078dda28359f945364e92988e060716a9d61

---
+++ background.js
@@ -0,0 +1,33 @@
+let background_img = ['video/EP03.mp4','video/ingame.mp4','video/1.mp4','video/2.mp4','video/3.mp4','video/noCRT.mp4','video/4.mp4']
+let current = 0
+function movingArrow(move){
+    if(move == 'rightArrow'){
+        current = (current+1) % background_img.length
+    }
+    else if(move == 'leftArrow'){
+        current = (current-1 + background_img.length) % background_img.length
+    }
+    document.getElementById('myVideo').classList.add('blur')
+    setTimeout(()=>{
+        document.getElementById('myVideo').src = background_img[current];
+        document.getElementById('myVideo').load();    
+        document.getElementById('myVideo').play();
+        if(document.getElementById('blur').checked){
+            document.getElementById('blur').checked = true
+        }
+        else{
+            document.getElementById('myVideo').classList.remove('blur');
+        }
+    },500)
+}
+document.getElementById('rightArrow').addEventListener('click',e=>(movingArrow('rightArrow')))
+document.getElementById('leftArrow').addEventListener('click',e=>(movingArrow('leftArrow')))
+document.addEventListener('keydown',e=>{
+    if(e.key == 'ArrowRight'){
+        movingArrow('rightArrow')
+    }
+    else if(e.key == 'ArrowLeft'){
+        movingArrow('leftArrow')
+    }
+
+})
